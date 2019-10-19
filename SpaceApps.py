6b044df67f132c98a6a98ef65724788c3dc7900e
Jian_Shan <l059069045@gmail.com>
2019-10-19 11:23:02 +0800

    second

--- SpaceApps.py
+++ SpaceApps.py
@@ -8,9 +8,9 @@ def cut(vx,vy):
     name1 =  "moon.jpg"
     im =Image.open(name1)
 
-    dx = 100 #偏移量
-    dy = 100
-    n = 1
+    dx = 160 #偏移量
+    dy = 160
+    n = 0
 
     x1 = 0  #從左上開始
     y1 = 0
@@ -44,7 +44,7 @@ if __name__=="__main__":
 
 def get_dominant_color(image):
     max_score = 0.0001
-    dominant_color = None
+    dominant_color = 0,0,0
     for count,(r,g,b) in image.getcolors(image.size[0]*image.size[1]): 
         #提高精確度
         saturation = colorsys.rgb_to_hsv(r/255.0, g/255.0, b/255.0)[1]
@@ -65,20 +65,13 @@ def get_dominant_color(image):
 a=0
 tmp=0
 if __name__ == '__main__':
-    for i in range (1,res+1):
+    for i in range (0,res+1):
         image = Image.open(str(i) + '.jpg')
         image = image.convert('RGB')
         rgb = get_dominant_color(image)
-        if rgb == None :
-            os.remove(str(i) +".jpg")
-        else:
-            if(i//37!=tmp):
-                a=0
-            tmp = i//37
-            a +=1
-            f = open('RGB.txt','a')
-            f.write(str(i//37+1)+"-"+str(a)+str(rgb)+"\n")
-            f.close()
+        f = open('RGB.txt','a')
+        f.write(str(i//23+1)+"-"+str(i%23)+str(rgb)+"\n")
+        f.close()
 
 
 
